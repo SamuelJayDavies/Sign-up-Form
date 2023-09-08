2cf131bc049f4ecdd9f773bb89ac07c887c86fe7
SamuelJayDavies <samj.davies@virginmedia.com>
2023-09-08 15:26:39 +0100

    Began working on back-end

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const email = document.querySelector("#email");
+const phoneNumber = document.querySelector("#phone-number");
+const password = document.querySelector("#password");
+const conPassword = document.querySelector("#con-password");
