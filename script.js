e070d1eb4db0d41105f85b43ae8380dac6c7eb06
SamuelJayDavies <samj.davies@virginmedia.com>
2023-09-22 18:13:37 +0100

    Began working on the validation and verification

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const firstName = document.querySelector("#first-name");
-const lastName = document.querySelector("#last-name");
-const email = document.querySelector("#email");
-const phoneNumber = document.querySelector("#phone-number");
-const password = document.querySelector("#password");
-const conPassword = document.querySelector("#con-password");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const phoneNumber = document.getElementById("#hone-number");
+const password = document.getElementById("password");
+const conPassword = document.getElementById("con-password");
+
+firstName.addEventListener("focusout", (event) => {
+    let content = event.target.value;
+    if(content == "") {
+        event.target.style.borderColor = "red";
+    } else {
+        event.target.style.borderColor = "rgba(128, 128, 128, 0.2)";
+    }
+})
+
+firstName.addEventListener("focusin", (event) => {
+    firstName.setAttribute("class", "focused")
+})
